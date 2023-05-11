0aefe8ba9170fc4eaf74604ac5f857a08263a357
NurHidayatiAsilah <nurhidayati.asilah24@gmail.com>
2023-05-11 17:01:20 +0700

    uts pbo

--- pengguna/User.java
+++ pengguna/User.java
@@ -1,5 +1,22 @@
 package pengguna;
 
 public class User {
+    private String username;
+    private String password;
 
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public String getUsername(){
+        return this.username;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public String getPassword(){
+        return this.password;
+    }
 }
